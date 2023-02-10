db4aa17b1e7c1a11700553ed2ce863f08320b10e
Mamta Gupta <mamta.gupta@godigitaltc.com>
2023-02-10 23:37:10 +0530

    promise, object inheritence and iterators implemented

---
+++ Week-4/Exercise1.js
@@ -0,0 +1,48 @@
+//! CUSTOM PROMISE 🙅
+
+
+function getNumber(resolveTime, rejectTime){
+    let randomNum = parseInt(Math.random()*100);
+    console.log(randomNum);
+
+    if (randomNum % 5 == 0){
+        // Thenable object
+        return {
+            then(){
+                setTimeout(() => console.log(`custom promise has been RESOLVED after ${resolveTime}ms`), resolveTime)
+            }
+        }
+    }
+    return {
+        // Thenable object
+        then(){
+            setTimeout(() => console.log(`custom promise has been REJECTED after ${rejectTime}ms`), rejectTime)
+        }
+    }
+}
+
+getNumber(1000, 2000).then();
+
+
+/**
+ * CAN BE DONE USING PROMISE AS 
+ * 
+ * 
+ * 
+function getNumber(resolveTime, rejectTime){
+    let randomNum = parseInt(Math.random()*100);
+    console.log(randomNum);
+    return new Promise((resolve, reject) => {
+        if(randomNum % 5 == 0){
+            resolve("resolved");
+        }
+        reject("rejected");
+    })
+}
+
+
+getNumber(1000, 2000)
+.then(console.log)
+.catch(console.log);
+
+ */
